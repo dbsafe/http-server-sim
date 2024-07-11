d11c241f04cb75ff13064495eeac9db6c99f8056
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2024-07-11 15:43:16 -0400

    HttpLoggingInterceptor - Consolidated logic.

--- HttpServerSim.App/Logger/HttpLoggingInterceptor.cs
+++ HttpServerSim.App/Logger/HttpLoggingInterceptor.cs
@@ -6,19 +6,19 @@ public sealed class HttpLoggingInterceptor : IHttpLoggingInterceptor
 {
     public ValueTask OnRequestAsync(HttpLoggingInterceptorContext logContext)
     {
-        AddHeaders(logContext);
+        AddHeaders(logContext.HttpContext.Request.Headers, logContext);
         return default;
     }
 
     public ValueTask OnResponseAsync(HttpLoggingInterceptorContext logContext)
     {
-        AddHeaders(logContext);
+        AddHeaders(logContext.HttpContext.Response.Headers, logContext);
         return default;
     }
 
-    private static void AddHeaders(HttpLoggingInterceptorContext logContext)
+    private static void AddHeaders(IHeaderDictionary headers, HttpLoggingInterceptorContext logContext)
     {
-        foreach (var header in logContext.HttpContext.Response.Headers)
+        foreach (var header in headers)
         {
             logContext.AddParameter(header.Key, header.Value);
         }
